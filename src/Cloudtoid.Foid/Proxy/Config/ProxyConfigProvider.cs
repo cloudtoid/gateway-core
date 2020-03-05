91629b2a37e1e580fd62cd9168b50a6ebb7a1d5a
Pedram Rezaei <rezaei.pedram@gmail.com>
2020-03-05 11:41:06 -0800

    Adding tests for proxy config provider

--- src/Cloudtoid.Foid/Proxy/Config/ProxyConfigProvider.cs
+++ src/Cloudtoid.Foid/Proxy/Config/ProxyConfigProvider.cs
@@ -1,28 +1,44 @@
 ﻿namespace Cloudtoid.Foid.Proxy
 {
     using System;
+    using System.Threading;
     using Microsoft.AspNetCore.Http;
     using Microsoft.Extensions.Configuration;
     using static Contract;
 
     internal sealed class ProxyConfigProvider : IProxyConfigProvider
     {
+        private readonly IConfiguration config;
         private Values values;
 
         public ProxyConfigProvider(IConfiguration config)
         {
-            CheckValue(config, nameof(config));
+            this.config = CheckValue(config, nameof(config));
             values = new Values(config);
-            config.GetReloadToken().RegisterChangeCallback(config => values = new Values((IConfiguration)config), config);
+            RegisterChangeCallback();
         }
+
+        internal AutoResetEvent ChangeEvent { get; } = new AutoResetEvent(false);
 
         public TimeSpan GetTotalTimeout(HttpContext context) => values.TotalTimeout;
 
+        private void RegisterChangeCallback()
+            => config.GetReloadToken().RegisterChangeCallback(_ => OnConfigChanged(), default);
+
+        private void OnConfigChanged()
+        {
+            values = new Values(config);
+            RegisterChangeCallback();
+            ChangeEvent.Set();
+        }
+
         private sealed class Values
         {
+            private const long TotalTimeoutDefault = 240000;
+
             internal Values(IConfiguration config)
             {
-                TotalTimeout = TimeSpan.FromMilliseconds(config.GetValue<long>(ConfigConstants.Proxy.TotalTimeoutInMillisecondsSection, 10000));
+                TotalTimeout = TimeSpan.FromMilliseconds(config.GetValue<long>(ConfigConstants.Proxy.TotalTimeoutInMillisecondsSection, TotalTimeoutDefault));
             }
 
             internal TimeSpan TotalTimeout { get; }
---
+++ tests/Cloudtoid.Foid.UnitTests/ProxyConfigProviderTests.cs
@@ -0,0 +1,70 @@
+﻿namespace Cloudtoid.Foid.UnitTests
+{
+    using System;
+    using System.IO;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Cloudtoid.Foid.Proxy;
+    using FluentAssertions;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Primitives;
+    using Microsoft.Net.Http.Headers;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public sealed class ProxyConfigProviderTests
+    {
+        [TestMethod]
+        public void InstantiateProvider_FullyPopulatedProxyConfig_AllValuesAreReadCorrectly()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("Configs\\ProxyConfig1.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            var provider = new ProxyConfigProvider(config);
+            provider.GetTotalTimeout(new DefaultHttpContext()).TotalMilliseconds.Should().Be(5000);
+        }
+
+        [TestMethod]
+        public void InstantiateProvider_NotFullyPopulatedProxyConfig_AllValuesHaveADefault()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("Configs\\ProxyConfigEmpty.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            var provider = new ProxyConfigProvider(config);
+            provider.GetTotalTimeout(new DefaultHttpContext()).TotalMilliseconds.Should().Be(240000);
+        }
+
+        [TestMethod]
+        public void InstantiateProvider_ConfigFileModified_FileIsReloaded()
+        {
+            try
+            {
+                File.Copy("Configs\\ProxyConfig1.json", "Configs\\ProxyConfigReload.json", true);
+
+                var config = new ConfigurationBuilder()
+                    .AddJsonFile("Configs\\ProxyConfigReload.json", optional: true, reloadOnChange: true)
+                    .Build();
+
+                var provider = new ProxyConfigProvider(config);
+                provider.GetTotalTimeout(new DefaultHttpContext()).TotalMilliseconds.Should().Be(5000);
+
+                File.Copy("Configs\\ProxyConfig2.json", "Configs\\ProxyConfigReload.json", true);
+                provider.ChangeEvent.WaitOne(2000);
+
+                provider.GetTotalTimeout(new DefaultHttpContext()).TotalMilliseconds.Should().Be(2000);
+
+                File.Copy("Configs\\ProxyConfig1.json", "Configs\\ProxyConfigReload.json", true);
+                provider.ChangeEvent.WaitOne(2000);
+
+                provider.GetTotalTimeout(new DefaultHttpContext()).TotalMilliseconds.Should().Be(5000);
+            }
+            finally
+            {
+                File.Delete("Configs\\ProxyConfigReload.json");
+            }
+        }
+    }
+}
