1d6078672079200b1ebdc2487c84a8369bb5f973
Pedram Rezaei <rezaei.pedram@gmail.com>
2020-03-06 10:22:37 -0800

    better config

--- src/Cloudtoid.Foid/Config/Constants.cs
+++ src/Cloudtoid.Foid/Config/Constants.cs
@@ -2,34 +2,34 @@
 {
     internal static class Constants
     {
-        private static readonly string Section = "foid";
+        private static readonly string Section = "foid:";
 
         internal static class Proxy
         {
-            private static readonly string Section = Constants.Section + ":proxy";
+            private static readonly string Section = Constants.Section + "proxy:";
 
             internal static class Upstream
             {
-                private static readonly string Section = Proxy.Section + ":upstream";
+                private static readonly string Section = Proxy.Section + "upstream:";
 
                 internal static class Request
                 {
-                    private static readonly string Section = Upstream.Section + ":request";
-                    internal static readonly string TimeoutInMilliseconds = Section + ":timeoutInMilliseconds";
+                    private static readonly string Section = Upstream.Section + "request:";
+                    internal static readonly string TimeoutInMilliseconds = Section + "timeoutInMilliseconds";
 
                     internal static class Headers
                     {
-                        private static readonly string Section = Request.Section + ":headers";
-                        internal static readonly string ProxyName = Section + ":proxyName";
-                        internal static readonly string DefaultHost = Section + ":defaultHost";
-                        internal static readonly string AllowHeadersWithEmptyValue = Section + ":allowHeadersWithEmptyValue";
-                        internal static readonly string AllowHeadersWithUnderscoreInName = Section + ":allowHeadersWithUnderscoreInName";
-                        internal static readonly string IncludeExternalAddress = Section + ":includeExternalAddress";
-                        internal static readonly string IgnoreClientAddress = Section + ":ignoreClientAddress";
-                        internal static readonly string IgnoreClientProtocol = Section + ":ignoreClientProtocol";
-                        internal static readonly string IgnoreRequestId = Section + ":ignoreRequestId";
-                        internal static readonly string IgnoreCallId = Section + ":ignoreCallId";
-                        internal static readonly string ExtraHeaders = Section + ":extraHeaders";
+                        private static readonly string Section = Request.Section + "headers:";
+                        internal static readonly string ProxyName = Section + "proxyName";
+                        internal static readonly string DefaultHost = Section + "defaultHost";
+                        internal static readonly string AllowHeadersWithEmptyValue = Section + "allowHeadersWithEmptyValue";
+                        internal static readonly string AllowHeadersWithUnderscoreInName = Section + "allowHeadersWithUnderscoreInName";
+                        internal static readonly string IncludeExternalAddress = Section + "includeExternalAddress";
+                        internal static readonly string IgnoreClientAddress = Section + "ignoreClientAddress";
+                        internal static readonly string IgnoreClientProtocol = Section + "ignoreClientProtocol";
+                        internal static readonly string IgnoreRequestId = Section + "ignoreRequestId";
+                        internal static readonly string IgnoreCallId = Section + "ignoreCallId";
+                        internal static readonly string ExtraHeaders = Section + "extraHeaders";
                     }
                 }
             }
