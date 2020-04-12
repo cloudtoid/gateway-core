90161c57951ddff85c28f2a03bc8a9206e07eee4
Pedram Rezaei <pedramr@microsoft.com>
2020-04-11 21:40:30 -0700

    More test coverage

--- src/Cloudtoid.GatewayCore/Headers/HeaderTypes.cs
+++ src/Cloudtoid.GatewayCore/Headers/HeaderTypes.cs
@@ -2,46 +2,25 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using Microsoft.Net.Http.Headers;
 
-    // The list below came from  System.Net.Http.Headers.KnownHeaders. See .net runtime on github.
+    // The list below came from System.Net.Http.Headers.KnownHeaders, System.Net.HttpRequestHeader and
+    // System.Net.HttpResponseHeader. See .NET runtime on github.
     internal static class HeaderTypes
     {
-        private static readonly ISet<string> GeneralHeaders = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+        private static readonly IList<string> GeneralHeaders = new List<string>()
         {
             HeaderNames.CacheControl,
             HeaderNames.Connection,
             HeaderNames.Date,
+            HeaderNames.KeepAlive,
             HeaderNames.Pragma,
             HeaderNames.Trailer,
             HeaderNames.TransferEncoding,
             HeaderNames.Upgrade,
             HeaderNames.Via,
             HeaderNames.Warning,
-        };
-
-        private static readonly ISet<string> RequestHeaders = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
-        {
-            HeaderNames.Accept,
-            HeaderNames.AcceptCharset,
-            HeaderNames.AcceptEncoding,
-            HeaderNames.AcceptLanguage,
-            "Alt-Used",
-            HeaderNames.Authorization,
-            HeaderNames.Expect,
-            HeaderNames.From,
-            HeaderNames.Host,
-            HeaderNames.IfMatch,
-            HeaderNames.IfModifiedSince,
-            HeaderNames.IfNoneMatch,
-            HeaderNames.IfRange,
-            HeaderNames.IfUnmodifiedSince,
-            HeaderNames.MaxForwards,
-            HeaderNames.ProxyAuthorization,
-            HeaderNames.Range,
-            HeaderNames.Referer,
-            HeaderNames.TE,
-            HeaderNames.UserAgent,
         };
 
         private static readonly ISet<string> ContentHeaders = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
@@ -59,7 +38,33 @@
             HeaderNames.LastModified,
         };
 
-        private static readonly ISet<string> ResponseHeaders = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+        private static readonly ISet<string> RequestHeaders = new HashSet<string>(GeneralHeaders.Concat(ContentHeaders), StringComparer.OrdinalIgnoreCase)
+        {
+            HeaderNames.Accept,
+            HeaderNames.AcceptCharset,
+            HeaderNames.AcceptEncoding,
+            HeaderNames.AcceptLanguage,
+            "Alt-Used",
+            HeaderNames.Authorization,
+            HeaderNames.Cookie,
+            HeaderNames.Expect,
+            HeaderNames.From,
+            HeaderNames.Host,
+            HeaderNames.IfMatch,
+            HeaderNames.IfModifiedSince,
+            HeaderNames.IfNoneMatch,
+            HeaderNames.IfRange,
+            HeaderNames.IfUnmodifiedSince,
+            HeaderNames.MaxForwards,
+            HeaderNames.ProxyAuthorization,
+            HeaderNames.Range,
+            HeaderNames.Referer,
+            HeaderNames.TE,
+            HeaderNames.Translate,
+            HeaderNames.UserAgent,
+        };
+
+        private static readonly ISet<string> ResponseHeaders = new HashSet<string>(GeneralHeaders.Concat(ContentHeaders), StringComparer.OrdinalIgnoreCase)
         {
             HeaderNames.Status,
             HeaderNames.AcceptRanges,
@@ -70,6 +75,7 @@
             HeaderNames.ProxyAuthenticate,
             HeaderNames.RetryAfter,
             HeaderNames.Server,
+            HeaderNames.SetCookie,
             HeaderNames.Vary,
             HeaderNames.WWWAuthenticate,
         };
@@ -111,8 +117,8 @@
             HeaderNames.WWWAuthenticate,
         };
 
-        internal static bool IsGeneralHeader(string headerName)
-            => GeneralHeaders.Contains(headerName);
+        internal static bool IsContentHeader(string headerName)
+            => ContentHeaders.Contains(headerName);
 
         internal static bool IsRequestHeader(string headerName)
             => RequestHeaders.Contains(headerName);
@@ -120,8 +126,8 @@
         internal static bool IsResponseHeader(string headerName)
             => ResponseHeaders.Contains(headerName);
 
-        internal static bool IsContentHeader(string headerName)
-            => ContentHeaders.Contains(headerName);
+        internal static bool IsNonTrailingHeader(string headerName)
+            => NonTrailingHeaders.Contains(headerName);
 
         internal static bool IsCustomHeader(string headerName)
             => headerName.StartsWithOrdinalIgnoreCase("x");
---
+++ tests/Cloudtoid.GatewayCore.UnitTests/Headers/HeadersTests.cs
@@ -0,0 +1,66 @@
+﻿namespace Cloudtoid.GatewayCore.UnitTests
+{
+    using System;
+    using System.Net;
+    using System.Reflection;
+    using Cloudtoid.GatewayCore.Headers;
+    using FluentAssertions;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.Primitives;
+    using Microsoft.Net.Http.Headers;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public sealed class HeadersTests
+    {
+        [TestMethod]
+        public void IsCustomHeader_StartsWithX_ItIs()
+        {
+            HeaderTypes.IsCustomHeader("x-test").Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void IsCustomHeader_NotStartWithX_ItIsNot()
+        {
+            HeaderTypes.IsCustomHeader("y-test").Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void IsNonTrailingHeader_Trailer_ItIs()
+        {
+            HeaderTypes.IsNonTrailingHeader(HeaderNames.Trailer).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void IsRequestHeader_ValidRequestHeaders_ItIs()
+        {
+            const BindingFlags Flags = BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Static;
+            foreach (var fieldName in Enum.GetNames(typeof(HttpRequestHeader)))
+            {
+                var headerName = (string)typeof(HeaderNames).GetField(fieldName, Flags)!.GetValue(null)!;
+                HeaderTypes.IsRequestHeader(headerName).Should().BeTrue();
+            }
+        }
+
+        [TestMethod]
+        public void IsRequestHeader_ValidResponseHeaders_ItIs()
+        {
+            const BindingFlags Flags = BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Static;
+            foreach (var fieldName in Enum.GetNames(typeof(HttpResponseHeader)))
+            {
+                var headerName = (string)typeof(HeaderNames).GetField(fieldName, Flags)!.GetValue(null)!;
+                HeaderTypes.IsResponseHeader(headerName).Should().BeTrue();
+            }
+        }
+
+        [TestMethod]
+        public void AddOrAppendHeaderValues_HeaderAlreadyExists_AppendsHeaderValue()
+        {
+            var headers = new HeaderDictionary();
+            headers.AddOrAppendHeaderValues("test", new[] { "value" });
+            headers.AddOrAppendHeaderValues("test", new[] { "new-value" });
+
+            headers["test"].Should().BeEquivalentTo(new[] { "value", "new-value" });
+        }
+    }
+}
